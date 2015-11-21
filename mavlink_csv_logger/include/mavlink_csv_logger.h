5068ba264d4cc7641a64c622d42bb574f1199d01
Markus Herb <mail@markusherb.de>
2015-11-21 22:16:39 +0100

    Implement csv logger

--- mavlink_csv_logger/include/mavlink_csv_logger.h
+++ mavlink_csv_logger/include/mavlink_csv_logger.h
@@ -4,11 +4,50 @@
 #include <lms/datamanager.h>
 #include <lms/module.h>
 
+#include <fstream>
+#include <tuple>
+#include <vector>
+#include <unordered_map>
+
+#include <mavlink/CC2016/mavlink.h>
+
 class MavlinkCsvLogger : public lms::Module {
 public:
     bool initialize();
     bool deinitialize();
     bool cycle();
+    
+protected:
+    //! LogMessageIdentifier tuple (sysid, msgid, compid)
+    typedef std::tuple<uint8_t, uint8_t, uint8_t> LogMessageIdentifier;
+    
+    struct LogMessageIdentifierHash : public std::unary_function<LogMessageIdentifier, std::size_t>
+    {
+        std::size_t operator()(const LogMessageIdentifier& id) const
+        {
+            return std::get<0>(id) + std::get<1>(id) + std::get<2>(id);
+        }
+    };
+    
+protected:
+    void logMessage(const mavlink_message_info_t& msgInfo, const mavlink_message_t& msg);
+    std::ofstream& getLogfile(const LogMessageIdentifier& id);
+    std::string generateFilename(const LogMessageIdentifier& id);
+    void printHeader(std::ofstream& out, uint8_t msgid);
+    
+    template<typename T>
+    void logField(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg);
+    
+protected:
+    //! Module config
+    const lms::ModuleConfig* config;
+    //! In data channel (messages received)
+    const std::vector<mavlink_message_t>* mavlinkChannel;
+    
+    //! Logfile prefix
+    std::string prefix;
+    //! LogMessageIdentifier -> logfile mapping
+    std::unordered_map<LogMessageIdentifier, std::ofstream, LogMessageIdentifierHash> files;
 };
 
 #endif // MAVLINK_CSV_LOGGER_H
--- mavlink_csv_logger/src/mavlink_csv_logger.cpp
+++ mavlink_csv_logger/src/mavlink_csv_logger.cpp
@@ -1,13 +1,233 @@
 #include "mavlink_csv_logger.h"
 
+#include <lms/extra/time.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+static mavlink_message_info_t msgInfos[256] = MAVLINK_MESSAGE_INFO;
+
 bool MavlinkCsvLogger::initialize() {
+    // Config
+    config = getConfig();
+    
+    auto directory = config->get<std::string>("directory");
+    bool createDateSubdirectory = config->get<bool>("create_subfolder_with_current_date");
+
+    if(directory.empty()) {
+        logger.error("init") << "directory is empty";
+        return false;
+    }
+    
+    if(createDateSubdirectory) {
+        directory = directory + "/" + lms::extra::currentTimeString();
+    }
+    
+    // create directory if not existing (ignoring any errors here)
+    mkdir(directory.c_str(), 0775);
+    
+    // Set log file prefix correctly
+    prefix = directory + "/";
+    
+    logger.debug("init") << "Log prefix: " << prefix;
+    
+    // Setup datachannels
+    auto channel = config->get<std::string>("channel");
+    mavlinkChannel = datamanager()->readChannel< std::vector<mavlink_message_t> >(this, channel);
+    
     return true;
 }
 
 bool MavlinkCsvLogger::deinitialize() {
+    for(auto& f : files)
+    {
+        // Close all opened log files
+        f.second.close();
+    }
+    
     return true;
 }
 
 bool MavlinkCsvLogger::cycle() {
+    for( const auto& msg : *mavlinkChannel )
+    {
+        // Log message
+        logMessage(msgInfos[msg.msgid], msg);
+    }
     return true;
 }
+
+void MavlinkCsvLogger::logMessage(const mavlink_message_info_t& msgInfo, const mavlink_message_t& msg)
+{
+    auto id = std::make_tuple(uint8_t(msg.sysid), uint8_t(msg.msgid), uint8_t(msg.compid));
+    auto& out = getLogfile(id);
+    
+    // Write new line to csv
+    for(size_t i = 0; i < msgInfo.num_fields; i++)
+    {
+        const mavlink_field_info_t& field = msgInfo.fields[i];
+        switch(field.type)
+        {
+            case MAVLINK_TYPE_CHAR:
+                logField<char>(out, field, msg);
+                break;
+            case MAVLINK_TYPE_UINT8_T:
+                logField<uint8_t>(out, field, msg);
+                break;
+            case MAVLINK_TYPE_INT8_T:
+                logField<int8_t>(out, field, msg);
+                break;
+            case MAVLINK_TYPE_UINT16_T:
+                logField<uint16_t>(out, field, msg);
+                break;
+            case MAVLINK_TYPE_INT16_T:
+                logField<int16_t>(out, field, msg);
+                break;
+            case MAVLINK_TYPE_UINT32_T:
+                logField<uint32_t>(out, field, msg);
+                break;
+            case MAVLINK_TYPE_INT32_T:
+                logField<int32_t>(out, field, msg);
+                break;
+            case MAVLINK_TYPE_UINT64_T:
+                logField<uint64_t>(out, field, msg);
+                break;
+            case MAVLINK_TYPE_INT64_T:
+                logField<int64_t>(out, field, msg);
+                break;
+            case MAVLINK_TYPE_FLOAT:
+                logField<float>(out, field, msg);
+                break;
+            case MAVLINK_TYPE_DOUBLE:
+                logField<double>(out, field, msg);
+                break;
+        }
+        
+        if( i < (msgInfo.num_fields-1))
+        {
+            out << ",";
+        }
+    }
+    out << std::endl;
+}
+
+
+std::string MavlinkCsvLogger::generateFilename(const LogMessageIdentifier& id)
+{
+    uint8_t sysid, msgid, compid;
+    std::tie(sysid, msgid, compid) = id;
+    
+    std::ostringstream filename;
+    filename << prefix << size_t(sysid) << "_" << msgInfos[msgid].name << "_" << size_t(compid) << ".csv";
+    return filename.str();
+}
+
+std::ofstream& MavlinkCsvLogger::getLogfile(const LogMessageIdentifier& id)
+{
+    auto file = files.find(id);
+    if(file == files.end())
+    {
+        // Create new log file
+        std::string filename = generateFilename(id);
+        files[id].open(filename);
+        if(!files[id])
+        {
+            logger.error() << "Unable to open output file " << filename << ": " << strerror(errno);
+        }
+        logger.warn() << "Opening logfile " << filename;
+        
+        // Print header fields
+        printHeader(files[id], std::get<1>(id));
+    }
+    
+    return files[id];
+}
+
+void MavlinkCsvLogger::printHeader(std::ofstream& out, uint8_t msgid)
+{
+    const mavlink_message_info_t& msgInfo = msgInfos[ msgid ];
+    for(size_t i = 0; i < msgInfo.num_fields; i++)
+    {
+        const mavlink_field_info_t& field = msgInfo.fields[i];
+        if( field.array_length == 0 || field.type == MAVLINK_TYPE_CHAR )
+        {
+            // Single-element or char array (string)
+            out << msgInfo.fields[i].name;
+        }
+        else
+        {
+            for( size_t j = 0; j < field.array_length; j++ )
+            {
+                out << msgInfo.fields[i].name << "[" << j << "]";
+                if( j < (field.array_length-1))
+                {
+                    out << ",";
+                }
+            }
+        }
+        
+        if( i < (msgInfo.num_fields-1))
+        {
+            out << ",";
+        }
+    }
+    out << std::endl;
+}
+
+
+template<typename T>
+void MavlinkCsvLogger::logField(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg)
+{
+    const T* ptr = reinterpret_cast<const T*>( reinterpret_cast<const uint8_t*>(&msg.payload64) + field.structure_offset );
+    if( field.array_length == 0 )
+    {
+        if( std::is_same<T, int8_t>::value )
+        {
+            // Force proper display format
+            out << static_cast<ssize_t>(*ptr);
+        }
+        else if( std::is_same<T, uint8_t>::value )
+        {
+            // Force proper display format
+            out << static_cast<size_t>(*ptr);
+        }
+        else
+        {
+            out << *ptr;
+        }
+    }
+    else
+    {
+        // Array
+        if( std::is_same<T, char>::value )
+        {
+            // Char array aka string
+            out << ptr;
+        }
+        else
+        {
+            // non-char array
+            for( size_t i = 0; i < field.array_length; i++ )
+            {
+                out << *(ptr+i);
+                if( i < (field.array_length-1))
+                {
+                    out << ",";
+                }
+            }
+        }
+    }
+}
+
+template void MavlinkCsvLogger::logField<char>(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg);
+template void MavlinkCsvLogger::logField<int8_t>(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg);
+template void MavlinkCsvLogger::logField<uint8_t>(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg);
+template void MavlinkCsvLogger::logField<int16_t>(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg);
+template void MavlinkCsvLogger::logField<uint16_t>(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg);
+template void MavlinkCsvLogger::logField<int32_t>(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg);
+template void MavlinkCsvLogger::logField<uint32_t>(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg);
+template void MavlinkCsvLogger::logField<int64_t>(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg);
+template void MavlinkCsvLogger::logField<uint64_t>(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg);
+template void MavlinkCsvLogger::logField<float>(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg);
+template void MavlinkCsvLogger::logField<double>(std::ofstream& out, const mavlink_field_info_t& field, const mavlink_message_t& msg);
