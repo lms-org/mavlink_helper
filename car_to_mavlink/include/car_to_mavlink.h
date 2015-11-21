d1ab7bd3b772ea74571b4ff7386aeab5a3b9b212
Markus Herb <mail@markusherb.de>
2015-11-21 22:56:31 +0100

    Implement car_to_mavlink

--- car_to_mavlink/include/car_to_mavlink.h
+++ car_to_mavlink/include/car_to_mavlink.h
@@ -4,11 +4,30 @@
 #include <lms/datamanager.h>
 #include <lms/module.h>
 
+#include <sensor_utils/car.h>
+
+#include <vector>
+
+#include <mavlink/CC2016/mavlink.h>
+
 class CarToMavlink : public lms::Module {
 public:
     bool initialize();
     bool deinitialize();
     bool cycle();
+    
+protected:
+    void checkHeartbeat();
+    void setControlCommands();
+    
+protected:
+    // Datachannels
+    const sensor_utils::Car* car;
+    const std::vector<mavlink_message_t>* inChannel;
+    std::vector<mavlink_message_t>* outChannel;
+    
+    // RC state
+    uint8_t lastRcState;
 };
 
 #endif // CAR_TO_MAVLINK_H
--- car_to_mavlink/src/car_to_mavlink.cpp
+++ car_to_mavlink/src/car_to_mavlink.cpp
@@ -1,6 +1,13 @@
 #include "car_to_mavlink.h"
 
 bool CarToMavlink::initialize() {
+    
+    // Datachannels
+    car = datamanager()->readChannel<sensor_utils::Car>(this,"CAR");
+    inChannel = datamanager()->readChannel< std::vector<mavlink_message_t> >(this, "MAVLINK_IN");
+    outChannel = datamanager()->writeChannel< std::vector<mavlink_message_t> >(this, "MAVLINK_OUT");
+
+    lastRcState = false;
     return true;
 }
 
@@ -9,5 +16,65 @@ bool CarToMavlink::deinitialize() {
 }
 
 bool CarToMavlink::cycle() {
+    
+    checkHeartbeat();
+    setControlCommands();
+    
     return true;
+}
+
+
+void CarToMavlink::checkHeartbeat()
+{
+    // Search inChannel for HEARTBEAT message
+    for( const auto& msg : *inChannel )
+    {
+        if( msg.msgid == MAVLINK_MSG_ID_HEARTBEAT )
+        {
+            // HEARTBEAT found
+            
+            // check rc state
+            auto currentRcState = mavlink_msg_heartbeat_get_remote_control(&msg);
+            if( currentRcState != lastRcState )
+            {
+                if( currentRcState == REMOTE_CONTROL_STATUS_DISCONNECTED )
+                {
+                    // Lost connection to remote control
+                    logger.error("cycle") << "Remote Control connection has been lost!";
+                } else if( currentRcState == REMOTE_CONTROL_STATUS_CONNECTED )
+                {
+                    if( lastRcState == REMOTE_CONTROL_STATUS_DISCONNECTED )
+                    {
+                        // Re-established connection
+                        logger.error("cycle") << "Remote Control connection has been re-established!";
+                    } else if( lastRcState == REMOTE_CONTROL_STATUS_ACTIVE )
+                    {
+                        // Switched off manual mode
+                        messaging()->send("RC_STATE_CHANGED", std::to_string(0));
+                        logger.warn("cycle") << "Remote Control has been switched to AUTONOMOUS mode!";
+                    }
+                } else
+                {
+                    // Switched into manual mode
+                    messaging()->send("RC_STATE_CHANGED", std::to_string(1));
+                    logger.warn("cycle") << "Remote Control has been switched to MANUAL mode!";
+                }
+                
+                // Update rc state
+                lastRcState = currentRcState;
+            }
+            
+            // check drive mode
+            // auto currentDriveMode = mavlink_msg_heartbeat_get_drive_mode(&msg);
+            // TODO!
+        }
+    }
+}
+
+void CarToMavlink::setControlCommands()
+{
+    mavlink_message_t msg;
+    // TODO: turn signal indicators
+    mavlink_msg_control_command_pack(0, 0, &msg, car->targetSpeed(), car->steeringFront(), car->steeringRear(), 0, 0);
+    outChannel->push_back(msg);
 }
