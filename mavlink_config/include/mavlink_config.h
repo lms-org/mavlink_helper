50ded92dfa7c47ded87ca96e0504badaa775c294
Markus Herb <mail@markusherb.de>
2015-11-27 13:30:45 +0100

    Add config_request_count to MavlinkConfig

--- mavlink_config/include/mavlink_config.h
+++ mavlink_config/include/mavlink_config.h
@@ -4,6 +4,9 @@
 #include <lms/datamanager.h>
 #include <lms/module.h>
 
+#include <vector>
+#include <mavlink/CC2016/mavlink.h>
+
 /**
  * @brief LMS module mavlink_config
  **/
@@ -12,6 +15,20 @@ public:
     bool initialize() override;
     bool deinitialize() override;
     bool cycle() override;
+
+protected:
+    void requestConfigCount();
+    void requestConfig(size_t config_id);
+    void requestParam(size_t config_id, size_t param_id);
+
+protected:
+    //! Out data channel
+    lms::WriteDataChannel< std::vector<mavlink_message_t> > outChannel;
+    //! In data channel
+    lms::ReadDataChannel< std::vector<mavlink_message_t> > inChannel;
+
+    //! Mavlink System ID
+    uint8_t systemID;
 };
 
 #endif // MAVLINK_CONFIG_H
--- mavlink_config/src/mavlink_config.cpp
+++ mavlink_config/src/mavlink_config.cpp
@@ -1,6 +1,13 @@
-#include "mavlink_config.h"
+#include <mavlink_config.h>
 
 bool MavlinkConfig::initialize() {
+    // Setup datachannels
+    inChannel = datamanager()->readChannel< std::vector<mavlink_message_t> >(this, "MAVLINK_IN");
+    outChannel = datamanager()->writeChannel< std::vector<mavlink_message_t> >(this, "MAVLINK_OUT");
+
+    // Read system id
+    systemID = config().get<size_t>("system_id");
+
     return true;
 }
 
@@ -9,5 +16,28 @@ bool MavlinkConfig::deinitialize() {
 }
 
 bool MavlinkConfig::cycle() {
+    // Request config count
+    requestConfigCount();
     return true;
 }
+
+void MavlinkConfig::requestConfigCount()
+{
+    mavlink_config_request_count_t request;
+    request.dummy = 0;
+
+    mavlink_message_t msg;
+    mavlink_msg_config_request_count_encode(systemID, 0, &msg, &request);
+    outChannel->push_back(msg);
+}
+
+void MavlinkConfig::requestConfig(size_t config_id)
+{
+
+}
+
+void MavlinkConfig::requestParam(size_t config_id, size_t param_id)
+{
+
+}
+
