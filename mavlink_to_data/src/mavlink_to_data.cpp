b85b6c3b471a7b6d9372ba2944d45edd9980f2fd
phibedy <philipp.schmette@gmx.de>
2017-02-02 21:11:52 +0100

    debug msg fix

--- mavlink_to_data/src/mavlink_to_data.cpp
+++ mavlink_to_data/src/mavlink_to_data.cpp
@@ -50,7 +50,7 @@ bool MavlinkToData::cycle() {
 void MavlinkToData::parseIncomingMessages(){
     int heartBeatMsgs = 0;
     for( const auto& msg : *inChannel ){
-        logger.debug("received channel sensor")<<msg.msgid;
+        logger.debug("received channel sensor")<<(int)msg.msgid;
         if(msg.msgid == MAVLINK_MSG_ID_HEARTBEAT){
             parseHeartBeat(msg);
             heartBeatMsgs++;
